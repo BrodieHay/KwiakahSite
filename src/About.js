bf3350439c05ca7d9f7884af7868f67cd59ad28e
Brodie Hay <brodiehay2334@gmail.com>
2021-03-11 09:56:29 -0800

    Centering images on about page, added rows as well

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ import {Figure} from 'react-bootstrap';
 function About(props) {
   return (
     <div>
-      <div className="row mx-3">
+      <div className="row mx-3 mt-3 align-items-center">
         <div className="col mx-3">
           Kwiakah First Nation are Lekwala speaking peoples. The Kwiakah are identified as part of the Laich-Kwil-Tach. The Laich-Kwil-Tach are the southernmost speakers of this northern Wakashan language. The traditional material culture, subsistence, social organization, religious and ceremonial practices of the Kwakw ak a’wakw were extensively documented in the late 19th and early 20th century by Franz Boas. Kwiakah First Nation entered the Treaty Process in February 1994, and is now in Stage 4 of the six-stage process “Negotiating an agreement in principle” (AIP). The goal of stage 4 is to reach the major agreements that will form the basis of the treaty. The Nation is represented in Treaty negotiations by the Hamatla Treaty Society. Also, the Kwiakah are member of the Nanwakolas Council and the Kwakuitl District Council.
         </div>
@@ -22,7 +22,7 @@ function About(props) {
           </Figure>
         </div>
       </div>
-      <div className="row mx-3">
+      <div className="row mx-3 mt-3 align-items-center">
         <div className="col mx-3">
           <Figure>
             <Figure.Image
--- src/App.js
+++ src/App.js
@@ -16,40 +16,45 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <Image src={HeaderImage} fluid />
-      <div className="container">
-        <div className="bottom-left">Kwiakah First Nation</div>
-        <Image className="bottom-right d-none d-md-block" src={Logo} fluid />
-      </div>
-
-      <Navbar bg="light" expand="sm">
-        <Navbar.Toggle className="mx-auto" aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <NavDropdown title="About" id="about-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/">Info</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/">Different Info</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link as={Link} to="/Treaty">Treaty</Nav.Link>
-            <Nav.Link as={Link} to="/Woodlots">Woodlots</Nav.Link>
-            <Nav.Link as={Link} to="/Tourism">Tourism</Nav.Link>
-            <NavDropdown title="Conservation" id="conservation-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/Conservation">Info</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/Conservation">Different Info</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to="#">Even More Info</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link as={Link} to="/ContactUs">ContactUs</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Route exact path="/" component={About} />
-      <Route exact path="/Treaty" component={Treaty} />
-      <Route exact path="/Woodlots" component={Woodlots} />
-      <Route exact path="/Tourism" component={Tourism} />
-      <Route exact path="/Conservation" component={Conservation} />
-      <Route exact path="/ContactUs" component={ContactUs} />
+        <div className="row">
+          <Image className="header-image" src={HeaderImage} fluid />
+          <div className="col">
+            <div className="top-left">Kwiakah First Nation</div>
+          </div>
+          <div className="col">
+            <Image className="top-right d-none d-md-block" src={Logo} fluid />
+          </div>
+        </div>
+        <div className="row">
+          <Navbar bg="light" expand="sm">
+            <Navbar.Toggle className="mx-auto" aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <NavDropdown title="About" id="about-nav-dropdown">
+                  <NavDropdown.Item as={Link} to="/">Info</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/">Different Info</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link as={Link} to="/Treaty">Treaty</Nav.Link>
+                <Nav.Link as={Link} to="/Woodlots">Woodlots</Nav.Link>
+                <Nav.Link as={Link} to="/Tourism">Tourism</Nav.Link>
+                <NavDropdown title="Conservation" id="conservation-nav-dropdown">
+                  <NavDropdown.Item as={Link} to="/Conservation">Info</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/Conservation">Different Info</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item as={Link} to="#">Even More Info</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link as={Link} to="/ContactUs">ContactUs</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Route exact path="/" component={About} />
+          <Route exact path="/Treaty" component={Treaty} />
+          <Route exact path="/Woodlots" component={Woodlots} />
+          <Route exact path="/Tourism" component={Tourism} />
+          <Route exact path="/Conservation" component={Conservation} />
+          <Route exact path="/ContactUs" component={ContactUs} />
+        </div>
       </div>
     </BrowserRouter>
   );
